3026442e66aa79cce2f9285aa7c465031188f282
Emuriuk <eastadym@gmail.com>
2023-06-19 16:53:58 -0700

    The success combination in business is: Do what you do better... and: do more of what you do

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,7 +7,7 @@
  * Return: Always 0 (success)
  */
 
-int main (void)
+int main(void)
 
 {
 	int d, p, q;
@@ -17,7 +17,6 @@ int main (void)
 
 	for (p = d + 1; p <= '9'; p++)
 	{
-	
 	for (q = p + 1; q <= '9'; q++)
 	{
 	if ((p != d) != q)
